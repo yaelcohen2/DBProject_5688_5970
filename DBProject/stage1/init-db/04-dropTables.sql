0e9071ac5edf491c7793a986886132082f3439d5
Yael Cohen <chnyael@g.jct.ac.il>
2026-03-19 15:55:04 +0200

    dropTables

---
+++ DBProject/stage1/init-db/04-dropTables.sql
@@ -0,0 +1,15 @@
+-- 1. First, drop relationship and log tables (which depend on tasks, employees, and supplies)
+DROP TABLE IF EXISTS USES;
+DROP TABLE IF EXISTS BELONGSTO;
+DROP TABLE IF EXISTS CLEANNINGLOG;
+DROP TABLE IF EXISTS ROOMCHECK;
+
+-- 2. Next, drop the main transaction table (which depends on rooms, statuses, and types)
+DROP TABLE IF EXISTS HOUSEKEPINGTASK;
+
+-- 3. Finally, drop the base lookup and entity tables (which have no foreign keys)
+DROP TABLE IF EXISTS CLEANNINGSUPPLIES;
+DROP TABLE IF EXISTS HOUSEKEEPINGEMPLOYEE;
+DROP TABLE IF EXISTS HOUSEKEEPINGSTATUS;
+DROP TABLE IF EXISTS TASKTYPE;
+DROP TABLE IF EXISTS ROOM;
